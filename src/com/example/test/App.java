fb43af61ffd8a76e8d0456bfafed4de1f10c06f3
66547109 <66547109@qq.com>
2015-08-24 13:50:04 +0800

    aa

---
+++ src/com/example/test/App.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+import android.app.Application;
+
+public class App extends Application{
+	private static App instance;
+	
+	public static App getInstance(){
+		return instance;
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		instance = this;
+		
+		init();
+	}
+
+	private void init() {
+		
+	}
+}
